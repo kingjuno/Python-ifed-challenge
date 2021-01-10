77ddba1d5670161dd652dcaf9a8384c3f2401842
Geo Jolly <69108486+kingjuno@users.noreply.github.com>
2021-01-10 15:20:15 +0530

    Completed Extra Challenge

---
+++ pokedex.py
@@ -0,0 +1,94 @@
+import PySimpleGUI as sg
+import requests
+
+
+layout = [[sg.Text("Enter the name of Pokemon:")],
+          [sg.Input(key='-INPUT-')],
+          [sg.Text(size=(100,1), key='-OUTPUT1-')],
+          [sg.Text(size=(100,1), key='-OUTPUT2-')],
+          [sg.Text(size=(100,3), key='-OUTPUT3-')],
+          [sg.Text(size=(100,15), key='-OUTPUT4-')],
+          [sg.Text(size=(100,1), key='-OUTPUT5-')],
+          [sg.Button('Search'), sg.Button('Quit')]]
+
+def ability(name):
+    url = "http://pokeapi.co/api/v2/pokemon/"+name+"/"
+    data = requests.get(url).json()
+    ably=[]
+    for i in data["abilities"]:
+        ably.append(i['ability']['name'])
+    return ably
+    
+
+
+window = sg.Window('PokeDex', layout)
+
+while True:
+    event, values = window.read()
+    if event == sg.WINDOW_CLOSED or event == 'Quit':
+        break
+    
+    Details="Name of the pokemon:"+values['-INPUT-']
+    window['-OUTPUT1-'].update(Details) 
+    name=values["-INPUT-"]
+    #####################################TYPE############################################################
+    urll = "http://pokeapi.co/api/v2/pokemon/"+name+"/"
+    urls=[]
+    tp=[]
+    data = requests.get(urll).json()
+    for i in range (2):
+        poke_type=data["types"][i]['type']['name']
+        urls.append(data["types"][i]['type']['url']) #appending url for step 2
+        tp.append(poke_type)
+
+    Details="Type of pokemon is: "+str(tp)
+    window['-OUTPUT2-'].update(Details)
+    
+    ################################DOUBLE DAMAGE#########################################################
+    ddm=['','']
+    ddm[0]+=tp[0]
+    ddm[1]+=tp[1]
+    
+    d_dmg_url=[]
+    tp=[]
+    ans1=[]
+    ans2=[]
+    cn=0
+    for url in urls:
+        poke_type=requests.get(url).json()
+        for j in poke_type["damage_relations"]["double_damage_from"]:
+            if not cn:
+                ans1.append(j["name"])
+            else:
+                ans2.append(j["name"])
+            tp.append(j["name"])
+            d_dmg_url.append(j["url"])
+            cn+=1
+    Details=''
+    Details="Double Damage are given by the following type: \n"
+    Details+="For "+str(ddm[0])+" : "+str(ans1)+"\n"
+    Details+="For "+str(ddm[1])+" : "+str(ans2)+"\n"
+    window['-OUTPUT3-'].update(Details)
+    
+    ######################################List 5 pokemon###################################################
+    Details='List 5 pokemons which gives the given pokemon double damage:\n'
+    for i in range(len(d_dmg_url)):
+        dat=requests.get(d_dmg_url[i]).json()
+        Details+=("Pokemon of Type: "+tp[i]+'\n')
+        for j in range(5):
+            Details+=(dat["pokemon"][j]["pokemon"]["name"])
+            if j!=4:
+                Details+=(',')
+        Details+=("\n\n")
+    
+    window['-OUTPUT4-'].update(Details)
+    ##################################ABILITY###############################################################
+    Details="Ability: "+str(ability(values['-INPUT-']))
+    window['-OUTPUT5-'].update(Details)
+
+window.close()
+
+# What is the type of pokemon
+# What type of pokemon gives double damages to the given pokemon
+# List 5 pokemons which gives the given pokemon double damage
+# Abilities of our pokemon
